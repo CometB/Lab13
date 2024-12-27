e0b92c4e6925fc0aa0f219b0c541fd19992afbc4
CometB <126880018+CometB@users.noreply.github.com>
2024-12-27 15:32:05 +0700

    .

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,23 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool image[][M], int size, int x, int y){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<M;j++){
+             if(sqrt(pow((i-x),2) + pow((j-y),2)) <= size-1) image[i][j] = 1;
+        }
+    }
+}
+
+void showImage(const bool image[][M]){
+    for(int i=-1;i<=N;i++){
+        for(int j=-1;j<=M;j++){
+            if(i == -1 || i == N) cout << '-';
+            else if(j == -1 || j == M) cout << '|';
+            else if(image[i][j]) cout << '*';
+            else cout << ' ';
+        }
+        cout << '\n';
+    }
+}
