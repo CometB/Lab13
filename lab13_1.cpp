84fe126db2b67cce32bf9033f8bac6a432e79374
CometB <126880018+CometB@users.noreply.github.com>
2024-12-27 14:59:23 +0700

    .

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,28 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double data[], int n, double result[]){
+    for(int i=0;i<6;i++){
+        result[i] = data[0];   
+    }
+    result[1] = 0;
+    result[3] = 1.0/data[0];
+    
+    for(int i=1;i<n;i++){
+        result[0] += data[i]; // Arithmetic Mean
+        result[2] *= data[i]; // Geometric Mean
+        result[3] += (1.0/data[i]); // Harmonic Mean
+        if(result[4] < data[i]) result[4] = data[i]; // Max
+        if(result[5] > data[i]) result[5] = data[i]; // Min
+    }
+    
+    result[0] = result[0]/n; // Arithmetic Mean
+    result[2] = pow(result[2],1.0/n); // Geometric Mean
+    result[3] = n/result[3]; // Harmonic Mean
+    
+    for(int i=0;i<n;i++){
+        result[1] += pow((data[i]-result[0]),2)/n; // Standard Deviation
+    }
+    result[1] = sqrt(result[1]); // Standard Deviation
+}
